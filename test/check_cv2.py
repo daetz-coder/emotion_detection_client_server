0027725a0df1805632a693f853cf6ccdf0087437
zhanyong <3446914219@qq.com>
2024-11-03 10:33:09 +0800

    del old version files

--- test/check_cv2.py
+++
@@ -1,9 +0,0 @@
-# import cv2
-# print(cv2.getBuildInformation())
-
-import cv2
-print(cv2.__version__)
-img = cv2.imread('path_to_an_image.jpg')
-cv2.imshow('Test Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
